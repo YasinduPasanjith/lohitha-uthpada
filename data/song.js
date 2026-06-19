a33483ad0afc8308306231636af75b3e262caeae
Yasindu Pasanjith <ypasanjith@gmail.com>
2026-06-20 04:56:32 +0530

    update

--- data/song.js
+++ data/song.js
@@ -8,6 +8,14 @@ export const mp3 = [
     name: 'A Thousand Years',
     url: '/audio/michael-jakson.mp3'
   },
+  { 
+    name: 'Nuhansi Sad song',
+    url: '/audio/nuhansi-sad-song.mp3'
+  },
+  {
+    name: 'Isuri- Song',
+    url: '/audio/Isuri-George-Michael.mp3'
+  },
   {
     name: 'Hen Sound',
     url: '/audio/hen-sound.mp3'
